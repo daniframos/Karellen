9a9b4091c254c7458de09897175d97e9364b0e43
Daniel Ramos <ramos.danielferreira@outlook.com>
2016-10-30 12:09:59 -0200

    Tradução via controller

--- Karellen.Web/Controllers/BaseController.cs
+++ Karellen.Web/Controllers/BaseController.cs
@@ -1,9 +1,19 @@
-﻿using System.Web.Mvc;
+﻿using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace Karellen.Web.Controllers
 {
     public class BaseController : Controller
     {
+        protected override void Initialize(RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+            var culture = new CultureInfo("pt-BR");
+            System.Threading.Thread.CurrentThread.CurrentCulture = culture;
+            System.Threading.Thread.CurrentThread.CurrentUICulture = culture;
+        }
+
         protected ContentResult GeoJson(string data)
         {
             return Content(data, "application/json");
